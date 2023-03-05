c0141546130c83d3966f3475b0c1e67c702661dd
itsashishrath <anandashish654@gmail.com>
2023-03-05 16:26:33 +0530

    Task 6

---
+++ Code/SB_3320_Task6/SB_3320_Run_motor.v
@@ -0,0 +1,32 @@
+module SB_3320_Run_motor(
+	input  reset,
+	input  clk_50,
+	output l_motor,
+	output r_motor,
+	output gndl,
+	output gndr,
+	input sensor_l,
+	input sensor_m,
+	input sensor_r,
+	output [2:0]turn,
+	output motor_stopped
+	);
+	
+		wire [2:0]stop		=3'b000;
+		wire [2:0]forward =3'b001;
+		wire [2:0]left		=3'b010;
+		wire [2:0]right	=3'b011;
+	
+		
+		
+		
+	   assign motor_stopped =(turn==stop)?1:0;
+											
+		SB_3320_line_run line_run_obj( .clk_50(clk_50), .sensor_1(sensor_l), .sensor_2(sensor_m), .sensor_3(sensor_r) , .turn(turn) );
+		
+		
+		SB_3320_motor_control motor_control_obj1(.clk_50(clk_50), .turn(turn), .left_motor(l_motor), .right_motor(r_motor) );
+				
+		
+		
+endmodule 
---
+++ Code/SB_3320_Task6/SB_3320_motor_control.v
@@ -0,0 +1,93 @@
+module SB_3320_motor_control (
+	 input clk_50,
+    input [2:0]turn,
+    output left_motor,
+    output right_motor,
+	 output gndl,
+	 output gndr
+    
+);
+
+reg [2:0]turn_out;
+reg [2:0]stop		  =3'b000;
+reg [2:0]forward    =3'b001;
+reg [2:0]left		  =3'b010;
+reg [2:0]right		  =3'b011;
+reg [2:0]extreme    =3'b100;
+
+
+wire [7:0]duty_l;
+wire [7:0]duty_r;
+wire [7:0]duty_gl;
+wire [7:0]duty_gr;
+
+reg [7:0]duty_lt;
+reg [7:0]duty_rt;
+reg [7:0]duty_glt;
+reg [7:0]duty_grt;
+
+
+always @(posedge clk_50) begin
+    case(turn)
+
+        forward 	 :		begin
+								duty_lt=8'd83;
+								duty_rt=8'd83;
+								duty_glt=8'd0;
+								duty_grt=8'd0;	
+								
+        end
+
+        left       :    begin
+								duty_lt =8'd0;
+								duty_rt =8'd83;
+								duty_glt=8'd83;
+								duty_grt=8'd0;
+        end
+
+        right      :    begin
+								duty_lt =8'd83;
+								duty_rt =8'd0;
+								duty_glt=8'd0;
+								duty_grt=8'd83;
+        end
+
+        extreme    : 	begin
+								duty_lt =8'd0;
+								duty_rt =8'd77;
+								duty_glt=8'd84;
+								duty_grt=8'd0;
+		  end
+
+		  stop       :    begin
+								duty_lt=8'd0;
+								duty_rt=8'd0;
+								duty_glt=8'd0;
+								duty_grt=8'd0;
+        end
+		  
+		  
+		  default  	 :		begin
+								duty_lt=8'd0;
+								duty_rt=8'd0;
+								duty_glt=8'd0;
+								duty_grt=8'd0;
+		  end
+		  
+    endcase
+	 
+end
+
+assign duty_l=duty_lt;
+assign duty_r=duty_rt;
+assign duty_gl=duty_glt;
+assign duty_gr=duty_grt;
+
+SB_3320_PWM_Generator PWM_Generator_left    ( .clk_50(clk_50), .duty_cycle(duty_l),  .out(left_motor) );
+SB_3320_PWM_Generator PWM_Generator_right   ( .clk_50(clk_50), .duty_cycle(duty_r),  .out(right_motor));
+SB_3320_PWM_Generator PWM_Generator_gndleft ( .clk_50(clk_50), .duty_cycle(duty_gl), .out(gndl)       );
+SB_3320_PWM_Generator PWM_Generator_gndright( .clk_50(clk_50), .duty_cycle(duty_gr), .out(gndr)       );
+
+
+
+endmodule 
